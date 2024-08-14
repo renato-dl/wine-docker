9f89cb68b6a88a65583f308667c39ce57c60cadd
renato-dl <55213679+renato-dl@users.noreply.github.com>
2024-08-14 12:23:41 +0200

    Update to noble
    * Update build.sh to noble
    
    * Rebase to noble

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ghcr.io/linuxserver/baseimage-kasmvnc:ubuntujammy
+FROM ghcr.io/linuxserver/baseimage-kasmvnc:ubuntunoble
 
 ENV HOME /config
 ENV WINEPREFIX ${HOME}/prefix32
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get -y install wget
 mkdir -pm755 /etc/apt/keyrings
 wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
 apt-get update
 apt-get -y install winehq-stable
 apt-get -y full-upgrade
