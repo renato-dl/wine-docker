cc690e9eb512529ffcd9608bf328e2a51146aef4
renato-dl <r-dl@outlook.it>
2026-04-26 14:20:03 +0200

    Update to resolute, WoW64

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-dpkg --add-architecture i386
 apt-get update
 apt-get -y install wget
 mkdir -pm755 /etc/apt/keyrings
 wget -O - https://dl.winehq.org/wine-builds/winehq.key | gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
-wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/resolute/winehq-resolute.sources
 apt-get update
 apt-get -y install winehq-stable
 apt-get -y full-upgrade
